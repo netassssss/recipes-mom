04b39ba061e29502b511f69e208fc3ab93dc368b
netanamogoo <neta.seesam@namogoo.com>
2020-09-28 19:59:03 +0300

    Splitting to components

--- src/views/main/Overview.vue
+++ src/views/main/Overview.vue
@@ -6,68 +6,27 @@
         <div>Add recipe</div>
       </div>
     </mom-button>
-    <modal-content :is-open="isModalOpen"
-                   @close="closeModalAndReset"
-                   @apply="nextStep"
-                   :ok-text="currentButtons.okText"
-                   :cancel-text="currentButtons.cancelText">
-      <template slot="header">
-        <div>{{ title }}</div>
-      </template>
-      <template slot="body">
-        <div v-for="(comp, index) in currentComponents"
-             :key="index"
-             class="components-container">
-          <component :is="comp"/>
-        </div>
-      </template>
-    </modal-content>
+    <steps :is-modal-open="isModalOpen" @close="closeModal"/>
   </div>
 </template>
 
 <script>
 import MomButton from '../../components/MomButton.vue';
-import modalMixin from '../../mixins/modalMixin';
 import recipeIcon from '../../../static/img/recipeIcon.png';
+import modalMixin from '../../mixins/modalMixin';
 
-import ModalContent from '../../components/addons/ModalContent.vue';
-import AddRecipe from '../../widgets/addons/AddRecipeContent.vue';
-
-import { steps } from './const';
+import Steps from '../../widgets/addons/Steps.vue';
 
 export default {
-  components: {
-    AddRecipe,
-    MomButton,
-    ModalContent,
-  },
   mixins: [modalMixin],
+  components: {
+    Steps,
+    MomButton,
+  },
   data() {
     return {
-      steps,
-      step: 0,
       recipeIcon,
     };
-  },
-  computed: {
-    title() {
-      return steps[this.step] ? steps[this.step].title : '';
-    },
-    currentComponents() {
-      return steps[this.step] ? steps[this.step].features : [];
-    },
-    currentButtons() {
-      return steps[this.step] ? steps[this.step].buttons : { okText: '', cancelText: '' };
-    },
-  },
-  methods: {
-    nextStep() {
-      this.step += 1;
-    },
-    closeModalAndReset() {
-      this.step = 0;
-      this.closeModal();
-    },
   },
 };
 </script>
@@ -86,14 +45,6 @@ export default {
     .overview-img {
       width: 20px;
       height: 20px;
-    }
-    .components-container {
-      padding: 20px;
-      width: 100%;
-      height: 100%;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
     }
   }
 </style>
---
+++ src/widgets/addons/Steps.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="steps-container">
+    <modal-content :is-open="isModalOpen"
+                   @close="closeModalAndReset"
+                   @apply="nextStep"
+                   :ok-text="currentButtons.okText"
+                   :cancel-text="currentButtons.cancelText">
+      <template slot="header">
+        <div>{{ title }}</div>
+      </template>
+      <template slot="body">
+        <div v-for="(comp, index) in currentComponents"
+             :key="index"
+             class="components-container">
+          <component :is="comp"/>
+        </div>
+      </template>
+    </modal-content>
+  </div>
+</template>
+
+<script>
+
+import ModalContent from '../../components/addons/ModalContent.vue';
+import AddRecipe from './AddRecipeContent.vue';
+
+import { steps } from './const';
+
+export default {
+  components: {
+    AddRecipe,
+    ModalContent,
+  },
+  props: {
+    isModalOpen: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      steps,
+      step: 0,
+    };
+  },
+  computed: {
+    title() {
+      return steps[this.step] ? steps[this.step].title : '';
+    },
+    currentComponents() {
+      return steps[this.step] ? steps[this.step].features : [];
+    },
+    currentButtons() {
+      return steps[this.step] ? steps[this.step].buttons : { okText: '', cancelText: '' };
+    },
+  },
+  methods: {
+    nextStep() {
+      this.step += 1;
+    },
+    closeModalAndReset() {
+      this.step = 0;
+      this.$emit('close');
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  .steps-container {
+    width: 100%;
+    height: 100%;
+    .components-container {
+      padding: 20px;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+</style>
